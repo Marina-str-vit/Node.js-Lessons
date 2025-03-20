2b01bb5fa4ecef30b878aa25ed81103db517bc60
Marina Strukova <meribsv@gmail.com>
2025-03-20 13:19:17 +0200

    Update writeSongs.js

--- lesson-2/nodejs-hw-01-template-main/src/utils/songs/writeSongs.js
+++ lesson-2/nodejs-hw-01-template-main/src/utils/songs/writeSongs.js
@@ -4,4 +4,6 @@ import fs from "node:fs/promises";
 
 import { PATH_DB_SONGS } from "../../constants/songs.js";
 
+// спочатку масив об'єктів перетворюємо на строку, а потім вже перезапусуємо дані, тому і використовуємо stringify
+// аргументи в stringify: null - ми не хочеми, щоб одні символи замінили на інші; 2 - відступи, щоб була нормальна кількість строк
 export const writeSongs = songs => fs.writeFile(PATH_DB_SONGS, JSON.stringify(songs, null, 2));
