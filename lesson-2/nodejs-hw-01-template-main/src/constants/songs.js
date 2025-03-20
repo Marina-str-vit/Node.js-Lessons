0f0c20afc920f9a9d246ec7c64df393ef2c6db00
Marina Strukova <meribsv@gmail.com>
2025-03-20 12:12:12 +0200

    Update songs.js

--- lesson-2/nodejs-hw-01-template-main/src/constants/songs.js
+++ lesson-2/nodejs-hw-01-template-main/src/constants/songs.js
@@ -2,11 +2,14 @@
 
 import path from "node:path";
 
-// оба варіанти вірні, але другий коротший
+// оба варіанти вірні для отримання абсолютного шляху, але другий коротший
 
 // 1 варіант)
 // export const PATH_DB_SONGS = path.join(process.cwd(), "src", "db", "db-songs.json");
 
 // 2 варіант отримання PATH_DB_SONGS
 export const PATH_DB_SONGS = path.resolve("src", "db", "db-songs.json");
+
+// для отримання відносного шляху 
+// export const PATH_DB_SONGS = path.join("src", "db", "db-songs.json");
 
