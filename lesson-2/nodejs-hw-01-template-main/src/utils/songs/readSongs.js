95d8ec1c68baf49bfad5cca1f7e1e755ab0af2a0
Marina Strukova <meribsv@gmail.com>
2025-03-20 12:20:40 +0200

    Update readSongs.js

--- lesson-2/nodejs-hw-01-template-main/src/utils/songs/readSongs.js
+++ lesson-2/nodejs-hw-01-template-main/src/utils/songs/readSongs.js
@@ -5,7 +5,8 @@ import {readFile} from "node:fs/promises";
 
 import { PATH_DB_SONGS } from "../../constants/songs.js";
 
-// "utf-8" перекодовує бінарний текст в строку
+// 1 - "utf-8" перекодовує бінарний текст в строку
+// 2 - прогнали текст через JSON.parse(data) і отримала масив об'єктів
 export const readSongs = async () => {
     const data = await readFile(PATH_DB_SONGS, "utf-8");
     return JSON.parse(data);
