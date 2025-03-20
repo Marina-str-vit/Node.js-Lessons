27a27eb486a69f22a4bf9f6638922afcfa9c95ef
Marina Strukova <meribsv@gmail.com>
2025-03-20 12:18:33 +0200

    Update readSongs.js

--- lesson-2/nodejs-hw-01-template-main/src/utils/songs/readSongs.js
+++ lesson-2/nodejs-hw-01-template-main/src/utils/songs/readSongs.js
@@ -1,9 +1,11 @@
 //  функ. що повторюються виносимо в папку utils!!
 
+// використ. в функ. проміс тому додаю "/promises" в імпорт
 import {readFile} from "node:fs/promises";
 
 import { PATH_DB_SONGS } from "../../constants/songs.js";
 
+// "utf-8" перекодовує шістнадцятковий текст в строку
 export const readSongs = async () => {
     const data = await readFile(PATH_DB_SONGS, "utf-8");
     return JSON.parse(data);
