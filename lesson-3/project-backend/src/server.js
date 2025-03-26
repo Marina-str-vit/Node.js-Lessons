aabf2768dda2ef9f6c6c8845cd1b3246cd279240
Marina Strukova <meribsv@gmail.com>
2025-03-26 18:09:09 +0200

    Update server.js

--- lesson-3/project-backend/src/server.js
+++ lesson-3/project-backend/src/server.js
@@ -1,3 +1,8 @@
+// 1 - імпортуємо express,
+// 2 - створюю вебсервер  const app = express();
+// 3 - прописуюю шлях відображення вебсторінки app.listen(port, ()=> console.log(`Server running on ${port} port`));
+// 3 - оформлюю запити  app.get("/", (req, res)
+
 import express from "express";
 import cors from "cors";
 import pino from "pino-http";
@@ -5,8 +10,9 @@ import pino from "pino-http";
 import { getEnvVar } from "./utils/getEnvVar.js";
 
 export const startServer = ()=> {
-    const app = express();
-
+    const app = express(); // це створили пустий сервер. Еxpress - бере перший запит і якщо добре!, то далі НЕ йде
+    // порядок перевірки коду зверху до низу
+ // використ middleware - ця функ. буде робити на будь-якому запиті
     app.use(cors());
     app.use(express.json());
     // app.use(pino({
@@ -14,13 +20,23 @@ export const startServer = ()=> {
     //         target: "pino-pretty"
     //     }
     // }));
-
+// це використ. коли необхідно, щоб запит пройшов деякі middleware, напр. зміна мови запиту, 
+   app.use((req, res, next)=> {
+       console.log("First middleware");
+//  щоб express рухався далі потрібно поставити виклик next() - каже, що треба шукати далі;
+        next();       
+   });
+    
+    // якщо прийде get-запит, то виконати наступну функцію
     app.get("/", (req, res)=> {
+        console.log(req.method); // щоб побачити яким методом прийшли дані
+        console.log(req.url); // щоб побичити звідки прийшло
         res.json({
             message: "Start work"
         });
     });
 
+  // тут не знайшов відповідні дані для запиту 
     app.use((req, res)=> {
         res.status(404).json({
             message: `${req.url} not found`
