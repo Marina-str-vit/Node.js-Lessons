7c8a6a0414289fd053194a250abd6fec852edb65
Marina Strukova <meribsv@gmail.com>
2025-04-13 22:27:36 +0300

    Update movies.js

--- lesson-7/project-backend/src/validation/movies.js
+++ lesson-7/project-backend/src/validation/movies.js
@@ -3,10 +3,15 @@ import Joi from "joi";
 import { typeList, minReleaseYear } from "../constants/movies.js";
 
 export const movieAddSchema = Joi.object({
+// якщо я хочу написати зрозуміло що саме трапилося то
+// title: Joi.string().required().messages({
+//   "any.required": "Требі вказати назву фільму",
+//    "String.base": "назва фільму має бути строкою", // якщо назву передали не строкою а ціфрою чи символом
+//})
     title: Joi.string().required(),
     director: Joi.string().required(),
     favorite: Joi.boolean(),
-    type: Joi.string().valid(...typeList),
+    type: Joi.string().valid(...typeList), // якщо маємо массив значень, то йду в константи і там беру постійний їх перелік
     releaseYear: Joi.number().min(minReleaseYear).required().messages({
         "number.min": `Movie before ${minReleaseYear} not found`
     }),
